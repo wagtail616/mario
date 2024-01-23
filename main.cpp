1f88ab272014dfbc199f5797c413bea4923d0eea
勇介　齊藤 <wisteriaq9@gmail.com>
2024-01-23 17:05:13 +0900

    メインループ完成

--- main.cpp
+++ main.cpp
@@ -10,23 +10,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
-
-	std::vector<const char*> ImageName{"images/ice_skate_man.png","images/ice_skate_kaiten.png"};
-
-
+	//画像
+	std::vector<const char*> ImageName{"ice_skate_man.png","ice_skate_kaiten.png"};
+	
 
 	//画像の読み込み
 	AssetLoader Loader;
 	Loader.Loading(ImageName);
 	//画像の表示
-	Actor actor(Loader.getValue(ImageName[1]));//Actorクラスのactorを宣言
-	actor.Draw();
+	Actor actor(Loader.getValue(ImageName[0]));//Actorクラスのactorを宣言
+
+	while (ProcessMessage() == 0) {
+
+		actor.Draw();
+
+		int end = CheckHitKey(KEY_INPUT_ESCAPE);
+		//ゲーム終了処理
+		if (end ==1) {
+			break;
+		}
+	}
 
 
 	//ジャンプしてる画像
 	//DrawGraph(200, 0, Loader.getID(1),TRUE);
 
-	WaitKey();				// キー入力待ち
 
 	DxLib_End();				// ＤＸライブラリ使用の終了処理
 
